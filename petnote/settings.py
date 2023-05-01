3a365c2c94c9079fd3e689c28097902c86933142
H. Attari <attari.hq@gmail.com>
2023-05-01 14:18:12 +0530

    linted_code_views_urls

--- petnote/settings.py
+++ petnote/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'petnote.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#
+#     }
+# }
 
 
 # Password validation
