e743638d0d0861f698a9a1765cb34b78adb87caa
H. Attari <attari.hq@gmail.com>
2023-05-01 14:18:12 +0530

    linted_code_views_urls

--- petnote/urls.py
+++ petnote/urls.py
@@ -13,40 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('home',views.home),
-    path('login',views.login),
-    path('sign', views.Register),
-    path('itask', views.rigstrtask),
-    path('check', views.logintask),
-    path('change', views.change),
-    path('logout',views.logout),
-    path('profile',views.profile),
-    path('chtask',views.changetask),
-    path('edit',views.edit),
-    path('edittask',views.edittask),
-    path('cart',views.cart),
-    path('reward',views.reward),
-    path('order',views.order),
-    path('about',views.about),
-    path('help',views.help),
-    path('helptask',views.helptask),
-    path('filter',views.filter),
-    path('search',views.search),
-    path('pay',views.pay),
-    path('success',views.success),
-    path('detail',views.detail),
-    path('add',views.addToCart),
-    path('forgot',views.forgot),
-    path('forgottask',views.forgottask),
-    path('admins/queries',views.admin_queries),
-    path('admins/orders',views.admin_orders),
-    path('admins/reply',views.reply),
-    path('admins/replytask',views.replytask)
-    #path('',views.),
+    path("", views.index),
+    path("home", views.home),
+    path("login", views.login),
+    path("sign", views.register),
+    path("itask", views.register_task),
+    path("check", views.login_task),
+    path("change", views.change),
+    path("logout", views.logout),
+    path("profile", views.profile),
+    path("chtask", views.change_task),
+    path("edit", views.edit),
+    path("edittask", views.edit_task),
+    path("cart", views.cart),
+    path("reward", views.reward),
+    path("order", views.order),
+    path("about", views.about),
+    path("help", views.request_help),
+    path("helptask", views.help_task),
+    path("filter", views.search_filter),
+    path("search", views.search),
+    path("pay", views.pay),
+    path("success", views.success),
+    path("detail", views.detail),
+    path("add", views.add_to_cart),
+    path("forgot", views.forgot),
+    path("forgottask", views.forgot_task),
+    path("admins/queries", views.admin_queries),
+    path("admins/orders", views.admin_orders),
+    path("admins/reply", views.query_reply),
+    path("admins/replytask", views.reply_task)
+    # path('',views.),
 ]
